774ffa61f8734f1fe8fdf359f65c760b49c606cd
gsingh993 <32495153+gsingh993@users.noreply.github.com>
2018-02-20 12:15:12 -0800

    Async Task

--- app/src/main/java/com/example/gurjitsingh3499/nytimesapp/MainActivity.java
+++ app/src/main/java/com/example/gurjitsingh3499/nytimesapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gurjitsingh3499.nytimesapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // The Async task will recieve 3 params -- String, Void, String
+    // String - String URL to RSS, Void -- Progress Bar, and String -- Result returns from RSS
+    private class DownloadData extends AsyncTask<String, Void, String> { //Use Void because we will not be using it
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
 }
