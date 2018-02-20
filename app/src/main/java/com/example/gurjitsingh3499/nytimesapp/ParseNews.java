6cc366ab5f88f8ef626de48ce21f50dae4cbecdf
gsingh993 <32495153+gsingh993@users.noreply.github.com>
2018-02-20 15:54:20 -0800

    Adding XML Processing

--- app/src/main/java/com/example/gurjitsingh3499/nytimesapp/ParseNews.java
+++ app/src/main/java/com/example/gurjitsingh3499/nytimesapp/ParseNews.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class ParseNews {
     private static final String TAG = "ParseNews";
-    private ArrayList<FeedEntry> news;
+    private ArrayList<FeedEntry> news; // Creates an Array list object, named new
 
     public ParseNews() {
         this.news = new ArrayList<>();
