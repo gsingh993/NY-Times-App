da22b28b37e4fecd6e7372d0f172da7b1e60a8bd
gsingh993 <32495153+gsingh993@users.noreply.github.com>
2018-02-20 15:30:11 -0800

    Adding Feed Entry

--- app/src/main/java/com/example/gurjitsingh3499/nytimesapp/FeedEntry.java
+++ app/src/main/java/com/example/gurjitsingh3499/nytimesapp/FeedEntry.java
@@ -2,4 +2,49 @@ package com.example.gurjitsingh3499.nytimesapp;
 
 
 public class FeedEntry {
+    private String name;
+    private String author;
+    private String pubDate;
+    private String summary;
+    private String imageUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(String pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
